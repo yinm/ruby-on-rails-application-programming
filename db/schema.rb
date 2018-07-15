72edeff9840f51c459d9818616dbc5cbc3b80fb2
yinm <yinm.cminor9@gmail.com>
2018-07-15 16:32:47 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_15_064708) do
+ActiveRecord::Schema.define(version: 2018_07_15_072103) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
