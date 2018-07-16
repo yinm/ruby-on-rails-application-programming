6db9b04834267ef46005e473ba57b0a9b548b28b
yinm <yinm.cminor9@gmail.com>
2018-07-16 09:31:59 +0900

    Add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ApplicationRecord
+  validates :isbn,
+    presence: true,
+    uniqueness: true,
+    length: { is: 17 },
+    format: { with: /\A[0-9]{3}-[0-9]-[0-9]{3, 5}-[0-9]{4}-[0-9X]\z/ }
+  validates :title,
+    presence: true,
+    length: { minimum: 1, maximum: 100 }
+  validates :price,
+    numericality: { only_integer: true, less_than: 10000 }
+  validates :publish,
+    inclusion: { in: ['技術評論社', '翔泳社', '秀和システム', '日経BP社', 'ソシム'] }
 end
