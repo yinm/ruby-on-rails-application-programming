3b0b636b0980977bd36f5070040e5703d8387e3c
yinm <yinm.cminor9@gmail.com>
2018-07-16 12:22:08 +0900

    Check custom validate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
+  validate :isbn_valid?
   validates :isbn,
     presence: { message: 'は必須です' },
     uniqueness: { allow_blank: true, message: '%{value}は一意でなければなりません' },
-    length: { is: 17, allow_blank: true, message: '%{value}は%{count}桁でなければなりません' },
-    isbn: { allow_old: true }
+    length: { is: 17, allow_blank: true, message: '%{value}は%{count}桁でなければなりません' }
   validates :title,
     presence: true,
     length: { minimum: 1, maximum: 100 }
@@ -11,4 +11,10 @@ class Book < ApplicationRecord
     numericality: { only_integer: true, less_than: 10000 }
   validates :publish,
     inclusion: { in: ['技術評論社', '翔泳社', '秀和システム', '日経BP社', 'ソシム'] }
+
+  private
+
+  def isbn_valid?
+    errors.add(:isbn, 'は正しい形式ではありません。') unless isbn =~ /\A[0-9]{3}-[0-9]-[0-9]{3,5}-[0-9]{4}-[0-9X]\z/
+  end
 end
