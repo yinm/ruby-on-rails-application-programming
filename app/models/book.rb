7b2c9b7ea0dbbe9aa4a17af3de3bd2e03fce6238
yinm <yinm.cminor9@gmail.com>
2018-07-16 11:10:50 +0900

    Custom validate class

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
     presence: { message: 'は必須です' },
     uniqueness: { allow_blank: true, message: '%{value}は一意でなければなりません' },
     length: { is: 17, allow_blank: true, message: '%{value}は%{count}桁でなければなりません' },
-    format: { with: /\A[0-9]{3}-[0-9]-[0-9]{3, 5}-[0-9]{4}-[0-9X]\z/, allow_blank: true, message: '%{value}は正しい形式ではありません' }
+    isbn: true
   validates :title,
     presence: true,
     length: { minimum: 1, maximum: 100 }
