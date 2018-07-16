61ef63d2abb602b8991bc1a6e485d2074dd1d5a8
yinm <yinm.cminor9@gmail.com>
2018-07-16 10:22:26 +0900

    Check confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password_digest, :email, :dm, :roles, :reviews_count, :agreement)
+      params.require(:user).permit(:username, :password_digest, :email, :email_confirmation, :dm, :roles, :reviews_count, :agreement)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :agreement, acceptance: true
+  validates :email, confirmation: true
 end
