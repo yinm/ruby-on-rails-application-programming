440ec7b054d002ef17357894885357f28add6f6b
yinm <yinm.cminor9@gmail.com>
2018-07-16 10:51:44 +0900

    Check unless parameter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :agreement, acceptance: { on: :create }
-  validates :email, confirmation: true
+  validates :email, presence: { unless: :sendmail? }
+
+  def sendmail?
+    dm.blank?
+  end
 end
