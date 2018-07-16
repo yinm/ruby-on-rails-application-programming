630dc5bfc24153e8646797358506f6cec34afdbc
yinm <yinm.cminor9@gmail.com>
2018-07-16 15:13:53 +0900

    Check params with array

--- app/controllers/ctrl_controller.rb
+++ app/controllers/ctrl_controller.rb
@@ -2,4 +2,8 @@ class CtrlController < ApplicationController
   def para
     render plain: 'idパラメーター:' + params[:id]
   end
+
+  def para_array
+    render plain: 'categoryパラメーター:' + params[:category].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   post 'record/keywd_process'
 
   get 'ctrl/para(/:id)' => 'ctrl#para'
+  get 'ctrl/para_array'
 end
